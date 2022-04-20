f7463b3a077925e326564a343c6db0ed6650ac63
Juan-Carlos-Estevez-Vargas <juank2001estevez@gmail.com>
2022-04-20 10:02:09 -0500

    Agreagndo vistas de producto y controlador del mismo.

--- src/main/java/com/juan/estevez/app/controller/ProductController.java
+++ src/main/java/com/juan/estevez/app/controller/ProductController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/administrator")
-public class AdministratorController {
-
-	@GetMapping("")
-	public String home() {
-		return "administrator/home";
-	}
+@RequestMapping("/products")
+public class ProductController {
 	
+	@GetMapping("")
+	public String show() {
+		return "products/show";
+	}
+
 }
