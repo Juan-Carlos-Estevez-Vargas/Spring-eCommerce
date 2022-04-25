afa1d050c41fa252eaec5ce29b8d2b8f1b64ba1c
Juan-Carlos-Estevez-Vargas <juank2001estevez@gmail.com>
2022-04-25 10:02:43 -0500

    Mostrar la vista del producto al usuario.

--- src/main/java/com/juan/estevez/app/controller/HomeController.java
+++ src/main/java/com/juan/estevez/app/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.juan.estevez.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.juan.estevez.app.service.ProductService;
@@ -12,6 +15,8 @@ import com.juan.estevez.app.service.ProductService;
 @RequestMapping("/")
 public class HomeController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductService productService;
 	
@@ -19,6 +24,12 @@ public class HomeController {
 	public String Home(Model model) {
 		model.addAttribute("products", productService.findAll());
 		return "user/home";
+	}
+	
+	@GetMapping("productHome/{id}")
+	public String productHome(@PathVariable Integer id) {
+		LOGGER.info("Id del producto como parámetro {}", id);
+		return "user/productHome";
 	}
 	
 }
