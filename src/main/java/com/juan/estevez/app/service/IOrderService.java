9a45a5cce6d6046e88cab2c1ce92b0cfe73e45c6
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-05-30 17:22:13 -0500

    Método para generar el número de la orden de manera aleatoria.

--- src/main/java/com/juan/estevez/app/service/IOrderService.java
+++ src/main/java/com/juan/estevez/app/service/IOrderService.java
@@ -1,8 +1,12 @@
 package com.juan.estevez.app.service;
 
+import java.util.List;
+
 import com.juan.estevez.app.model.Order;
 
 public interface IOrderService {
+	
+	List<Order> findAll();
 
 	Order save(Order order);
 }
--- src/main/java/com/juan/estevez/app/service/OrderServiceImpl.java
+++ src/main/java/com/juan/estevez/app/service/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.juan.estevez.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,25 @@ public class OrderServiceImpl implements IOrderService {
 
 	@Autowired
 	private IOrderRepository orderRepository;
-	
+
 	@Override
 	public Order save(Order order) {
 		return orderRepository.save(order);
 	}
-	
-	
+
+	@Override
+	public List<Order> findAll() {
+
+		return orderRepository.findAll();
+	}
+
+	public String generateOrderNumber(int num) {
+		String numeroOrden = String.valueOf(num);
+		int numDigitos = numeroOrden.length(); // numero de digitos
+		for (int j = numDigitos; j <= 9; j++) // añadimos los ceros
+			numeroOrden = "0" + numeroOrden;
+
+		return numeroOrden;
+	}
 
 }
