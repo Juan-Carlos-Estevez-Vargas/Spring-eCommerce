344aaf555e2e44d332f0a6fd38ecf30e90f13a79
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-06-04 11:38:31 -0500

    Funcionalidad para registrar usuarios al sistema.

--- src/main/java/com/juan/estevez/app/controller/UserController.java
+++ src/main/java/com/juan/estevez/app/controller/UserController.java
@@ -3,8 +3,10 @@ package com.juan.estevez.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.juan.estevez.app.model.User;
 import com.juan.estevez.app.service.IUserService;
 
 @Controller
@@ -17,6 +19,13 @@ public class UserController {
 	@GetMapping("/registration")
 	public String create() {
 		return "user/registro";
+	}
+	
+	@PostMapping("/save")
+	public String save(User user) {
+		user.setType("USER");
+		userService.save(user);
+		return "redirect:/";
 	}
 	
 }
