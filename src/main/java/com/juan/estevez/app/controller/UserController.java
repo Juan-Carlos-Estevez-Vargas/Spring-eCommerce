c3963fe0b0fdd25988ff743649dac77b42058173
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-06-04 09:58:21 -0500

    Redireccionando a la vista de registro de usuarios.

--- src/main/java/com/juan/estevez/app/controller/UserController.java
+++ src/main/java/com/juan/estevez/app/controller/UserController.java
@@ -1,5 +1,22 @@
 package com.juan.estevez.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.juan.estevez.app.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private IUserService userService;
+	
+	@GetMapping("/registration")
+	public String create() {
+		return "user/registro";
+	}
+	
 }
