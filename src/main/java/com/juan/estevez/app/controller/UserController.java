f2ca31658b85b3e28a59b06f5b7e53989b37c0d2
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-06-05 19:20:54 -0500

    Vista compras.html para el usuario.

--- src/main/java/com/juan/estevez/app/controller/UserController.java
+++ src/main/java/com/juan/estevez/app/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,5 +52,11 @@ public class UserController {
 		}
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/compra")
+	public String obtenerCompras(HttpSession session, Model model) {
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		return "user/compras";
 	}
 }
